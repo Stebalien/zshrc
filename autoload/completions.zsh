67577e6dbc3b3d10c7c703978065a071d264520f
Steven Allen <steven@stebalien.com>
2016-11-27 13:48:44 -0800

    get rid of defunct completions

--- autoload/completions.zsh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/zsh
-
-_email-lbdbq() {
-    reply=(${(f)"$(notmuch-addrlookup $PREFIX | awk -F'\t' '(NR!=1) {print($2 ":" $1);}')"})
-    return 300
-}
-
-fpath=("$XDG_CONFIG_HOME/zsh/Completion" $fpath)
