b5a50d05c66a25719748153e987cbd16e086a881
Steven Allen <steven@stebalien.com>
2016-11-27 13:45:50 -0800

    fix export of manpage highlighting

--- autoload/color.zsh
+++ autoload/color.zsh
@@ -15,13 +15,13 @@ if [[ "$terminfo[colors]" -ge 8 ]]; then
         fi
     }
     # Less
-    LESS_TERMCAP_mb=$'\E[01;31m'
-    LESS_TERMCAP_md=$'\E[01;38;5;74m'
-    LESS_TERMCAP_me=$'\E[0m'
-    LESS_TERMCAP_se=$'\E[0m'
-    LESS_TERMCAP_so=$'\E[38;5;246m'
-    LESS_TERMCAP_ue=$'\E[0m'
-    LESS_TERMCAP_us=$'\E[04;38;5;146m'
+    export LESS_TERMCAP_mb=$'\E[01;31m'
+    export LESS_TERMCAP_md=$'\E[01;38;5;74m'
+    export LESS_TERMCAP_me=$'\E[0m'
+    export LESS_TERMCAP_se=$'\E[0m'
+    export LESS_TERMCAP_so=$'\E[38;5;246m'
+    export LESS_TERMCAP_ue=$'\E[0m'
+    export LESS_TERMCAP_us=$'\E[04;38;5;146m'
 
     export LS_COLORS="$LS_COLORS:di=36:*.swp=37:*.pyo=37:*.pyc=37:*.java=32:*.py=32:*.lua=32:*.c=32:*.html=32:*.css=32:*.js=32:*.rst=32"
     zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
